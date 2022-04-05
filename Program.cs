fc9b9428f5dadccd2d0d49a9bb3f3a4c1b91435a
Chibuzorchi <chinonsochibuzor2277@gmail.com>
2022-04-05 09:36:14 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloworldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from kinsmen!");
+        }
+    }
+}
